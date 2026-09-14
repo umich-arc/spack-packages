365eee61693dfbb0bb1dc8f216d4dea1a3eb3dd9
Andrew Robbins <andrew@robbinsa.me>
2026-09-14 15:20:43 +0000

    remove windows/mac runs

--- .ci/validate_last_exit.ps1
+++
@@ -1,3 +0,0 @@
-if ($LASTEXITCODE -ne 0){
-    throw "Tests have failed"
-}
--- .ci/windows_test_setup.ps1
+++
@@ -1,5 +0,0 @@
-$ErrorActionPreference = "SilentlyContinue"
-Write-Output F|xcopy .\spack-core\share\spack\qa\configuration\windows_config.yaml "$env:USERPROFILE\.spack\windows\config.yaml"
-# The line below prevents the _spack_root symlink from causing issues with cyclic symlinks on Windows
-(Get-Item '.\spack-core\lib\spack\docs\_spack_root').Delete()
-./spack-core/share/spack/setup-env.ps1
