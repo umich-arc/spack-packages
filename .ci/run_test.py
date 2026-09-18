d9944adfcd373db0560271ce9ecde139286c016a
Andrew Robbins <andrew@robbinsa.me>
2026-09-18 19:48:44 +0000

    tweak endpoint

--- .ci/run_test.py
+++ .ci/run_test.py
@@ -1,6 +1,6 @@
 from globus_compute_sdk import Executor, ShellFunction
 
-endpoint_id = "aed92014-81b1-44b7-b82a-68b21feeff44"
+endpoint_id = "113871fa-ecc5-4b54-af3a-1b5b0e7206bf"
 
 func = ShellFunction("spack audit packages", return_dict=True)
 with Executor(
