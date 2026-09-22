b6ed23d25154efcc4a7124020e5ba1e44f335437
Andrew Robbins <andrew@robbinsa.me>
2026-09-22 19:31:27 +0000

    dont use shellfunc

--- .ci/run_test.py
+++ .ci/run_test.py
@@ -1,11 +1,17 @@
 import os
 
-from globus_compute_sdk import Executor, ShellFunction
+from globus_compute_sdk import Executor
 from globus_compute_sdk.serialize import ComputeSerializer, PureSourceDill
 
 endpoint_id = "8c7f597d-af89-4af0-b4b5-75619ef793f1"
 
-func = ShellFunction("python3 -m pytest -v --exitfirst", return_dict=True)
+
+def func():
+    import pytest
+
+    return pytest.main(["-v", "--exitfirst"])
+
+
 with Executor(
     endpoint_id=endpoint_id,
     user_endpoint_config={"commit_sha": os.environ["COMMIT_SHA"]},
