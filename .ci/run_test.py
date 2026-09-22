813318793ef46ee47f0ff0c59af692e33082dfde
Andrew Robbins <andrew@robbinsa.me>
2026-09-22 16:10:51 -0400

    use the other correct serializer

--- .ci/run_test.py
+++ .ci/run_test.py
@@ -1,7 +1,7 @@
 import os
 
 from globus_compute_sdk import Executor
-from globus_compute_sdk.serialize import ComputeSerializer, PureSourceDill
+from globus_compute_sdk.serialize import ComputeSerializer, JSONData, PureSourceDill
 
 endpoint_id = "8c7f597d-af89-4af0-b4b5-75619ef793f1"
 
@@ -16,7 +16,7 @@ with Executor(
     endpoint_id=endpoint_id,
     user_endpoint_config={"commit_sha": os.environ["COMMIT_SHA"]},
 ) as ex:
-    ex.serializer = ComputeSerializer(strategy_code=PureSourceDill)
+    ex.serializer = ComputeSerializer(strategy_code=PureSourceDill, strategy_data=JSONData)
     fut = ex.submit(func)
     res = fut.result()
 
