103de2465331260f2cf9c8688f707b428fd41846
Andrew Robbins <andrew@robbinsa.me>
2026-09-22 18:06:34 +0000

    pytest?

--- .ci/run_test.py
+++ .ci/run_test.py
@@ -1,16 +1,17 @@
+import os
 from globus_compute_sdk import Executor, ShellFunction
 
 endpoint_id = "8c7f597d-af89-4af0-b4b5-75619ef793f1"
 
-func = ShellFunction("spack audit packages", return_dict=True)
+func = ShellFunction("python3 -m pytest -v --exitfirst", return_dict=True)
 with Executor(
     endpoint_id=endpoint_id,
     user_endpoint_config={
-        "OPTIONS": "--time 30",
-        "COMMAND": "source /sw/spack/arc/spack/share/spack/setup-env.sh",
+        "commit_sha": os.environ["COMMIT_SHA"]
     },
 ) as ex:
     fut = ex.submit(func)
     res = fut.result()
 
+print(res)
 # ADD RESULT PROCESSING LOGIC
