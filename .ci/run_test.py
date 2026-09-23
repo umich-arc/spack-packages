a13ab1b83649683edb8cd879e2b7ac017a2a587f
Andrew Robbins <andrew@robbinsa.me>
2026-09-23 18:44:34 +0000

    pass run ID

--- .ci/run_test.py
+++ .ci/run_test.py
@@ -17,6 +17,7 @@ with Executor(
     user_endpoint_config={
         "commit_sha": os.environ["COMMIT_SHA"],
         "gh_token": os.environ["GH_TOKEN"],
+        "run_id": os.environ["GITHUB_RUN_ID"] + "." + os.environ["GITHUB_RUN_ATTEMPT"],
     },
 ) as ex:
     ex.serializer = ComputeSerializer(strategy_code=PureSourceDill, strategy_data=JSONData)
